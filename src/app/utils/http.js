8c3e033318203e5aca376a8230328710e8c80416
tonyandy5630 <tonyandy456@gmail.com>
2023-12-22 00:28:51 +0700

    Add theme

---
+++ src/app/utils/http.js
@@ -0,0 +1,33 @@
+import axios, { AxiosError, HttpStatusCode } from "axios";
+import { toast } from "react-toastify";
+
+class Http {
+  constructor() {
+    this.instance = axios.create({
+      baseURL:
+        process.env.NODE_ENV === "development"
+          ? process.env.NEXT_PUBLIC_SERVER_URL
+          : process.env.NEXT_PUBLIC_PROD_SERVER_URL,
+      timeout: 10000,
+      withCredentials: true,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    this.instance.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (error.response?.status !== HttpStatusCode.UnprocessableEntity) {
+          const data = error.response?.data;
+          const message = data.message || error.message;
+          toast.error(message);
+        }
+        return Promise.reject(error);
+      }
+    );
+  }
+}
+
+const http = new Http().instance;
+export default http;
---
+++ src/app/utils/themes.js
@@ -0,0 +1,24 @@
+"use client";
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#D4FCF9",
+    },
+    secondary: {
+      main: "#FF9E9E",
+    },
+    error: {
+      main: "#E76F51",
+    },
+    success: {
+      main: "#57CC99",
+    },
+    warning: {
+      main: "#FF9800",
+    },
+  },
+});
+
+export default theme;
