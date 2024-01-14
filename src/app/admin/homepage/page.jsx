45aecc3f3c19c4760c45dbf818201e69aa5b888b
tonyandy5630 <tonyandy456@gmail.com>
2024-01-15 03:27:58 +0700

    [Tu] Add stat tabs in admin homepage

--- src/app/admin/homepage/page.jsx
+++ src/app/admin/homepage/page.jsx
@@ -1,6 +1,31 @@
 import AdminLayout from '@/components/Layout/Admin'
+import StatTab from '@/components/StatTab'
+import { HomePageStatTabs } from '@/constants/HomePage.const'
 import React from 'react'
-
+import Typography from '@mui/material/Typography'
 export default function HomePage() {
-    return <AdminLayout>HomePage</AdminLayout>
+    return (
+        <AdminLayout>
+            <div className="flex items-center justify-between min-w-full">
+                {HomePageStatTabs.map((item) => {
+                    return (
+                        <StatTab
+                            key={item.title}
+                            size="small"
+                            title={item.title}
+                            icon={item.icon}
+                            endAdornment={`${
+                                item ===
+                                HomePageStatTabs.at(HomePageStatTabs.length - 1)
+                                    ? 'VND'
+                                    : ''
+                            }`}
+                        >
+                            300.000.000
+                        </StatTab>
+                    )
+                })}
+            </div>
+        </AdminLayout>
+    )
 }
--- src/components/StatTab/index.jsx
+++ src/components/StatTab/index.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import Typography from '@mui/material/Typography'
 export default function StatTab(props) {
     return (
-        <div className="min-w-80 pl-4 space-x-3 h-20 bg-green rounded-2xl flex justify-start items-center">
+        <div
+            className={` ${
+                props.size === 'small' ? 'min-w-64' : 'min-w-80'
+            } pl-4 space-x-3 h-20 bg-green rounded-2xl flex justify-start items-center`}
+        >
             {props.icon}
-            <div className="flex flex-col justify-center items-start">
+            <div className="flex flex-col items-start justify-center">
                 <Typography
                     variant="subtitle1"
                     className="text-mosh"
@@ -12,7 +16,12 @@ export default function StatTab(props) {
                 >
                     {props.title}
                 </Typography>
-                <Typography className="text-white">{props.children}</Typography>
+                <Typography className="w-full text-white">
+                    <span className="inline-block mr-2 min-w-20 ">
+                        {props.children}
+                    </span>
+                    {props.endAdornment}
+                </Typography>
             </div>
         </div>
     )
