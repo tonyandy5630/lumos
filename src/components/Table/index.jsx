f2ed7a5afc5461306dd3cd9a5662f4faf92654ea
Bùi Thanh Tú <61198577+tonyandy5630@users.noreply.github.com>
2024-02-07 18:27:14 +0700

    <FolderStructure> Move page detail page component outside

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,7 +1,6 @@
 // @ts-nocheck
 'use client'
 import * as React from 'react'
-import { useMemo } from 'react'
 import { MaterialReactTable, useMaterialReactTable } from 'material-react-table'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
