5ab07bb0922ccc8d5506dc739472f3e99fee2540
Bùi Thanh Tú <61198577+tonyandy5630@users.noreply.github.com>
2024-02-22 00:08:25 +0700

    <module> add login api url

--- src/constants/API-URL/index.js
+++ src/constants/API-URL/index.js
@@ -1,3 +1,5 @@
 const supplier = 'supplier'
+const auth = 'auth'
 
 export const baseSuppliers = `${process.env.NEXT_PUBLIC_SERVER_URL}/${supplier}`
+export const baseAuth = `${process.env.NEXT_PUBLIC_SERVER_URL}/${auth}}`
