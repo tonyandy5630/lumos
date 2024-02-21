04583115036359c6e700722c3c7edd8da42f4b32
Bùi Thanh Tú <61198577+tonyandy5630@users.noreply.github.com>
2024-02-21 15:36:15 +0700

    <UI> Add admin bookings page

---
+++ src/app/admin/bookings/page.jsx
@@ -0,0 +1,6 @@
+import AdminLayout from '@/components/Layout/Admin'
+import React from 'react'
+
+export default function AdminBookingsPage() {
+    return <AdminLayout>Working on it...</AdminLayout>
+}
