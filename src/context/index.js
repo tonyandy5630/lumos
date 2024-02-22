ead1f991326ad51bb22f8745acb87b59c5d10549
Bùi Thanh Tú <61198577+tonyandy5630@users.noreply.github.com>
2024-02-22 21:24:00 +0700

    <bug> fix context dir bug on name

---
+++ src/context/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const defaultValue = {
+    user: null,
+}
+
+export const AuthContext = React.createContext(defaultValue)
+
+export const useAuth = () => React.useContext(AuthContext)
+
+export const AuthContextProvider = ({ children }) => {
+    const [user, setUser] = React.useState()
+
+    const value = { user, setUser }
+
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
