c0ae8b9572df554b9a2829e603c4b4e8e1eea645
Bùi Thanh Tú <61198577+tonyandy5630@users.noreply.github.com>
2024-02-23 02:45:41 +0700

    <ui> landing page show go to dashboard is logged in

--- src/components/Layout/LandingPage/Header/index.jsx
+++ src/components/Layout/LandingPage/Header/index.jsx
@@ -7,8 +7,15 @@ import Link from 'next/link'
 import HeaderTabs from './Tabs'
 import HideOnScroll from '../../HideOnScroll'
 import AUTH_URL from '@/constants/URL/auth'
+import { cookies } from 'next/headers'
+import { getCookie } from 'cookies-next'
+import { userRole_c } from '@/constants/cookies.const'
+import ROLES from '@/constants/roles.const'
+import ADMIN_URL from '@/constants/URL/admin'
+import NURSE_URL from '@/constants/URL/partner'
 
 export default function LandingPageHeader() {
+    const userRoles = getCookie(userRole_c, { cookies })
     return (
         <HideOnScroll>
             <AppBar className="!shadow-none h-28  flex justify-center items-center">
@@ -26,37 +33,64 @@ export default function LandingPageHeader() {
                     <div className="flex items-center justify-end w-fit">
                         <HeaderTabs />
                         <div className="flex items-center justify-between space-x-5 w-fit">
-                            <Button
-                                variant="outlined"
-                                LinkComponent={Link}
-                                href={AUTH_URL.LOGIN}
-                                sx={{
-                                    color: 'success.dark',
-                                    borderColor: 'success.dark',
-                                    borderRadius: '15px',
-                                    '&:hover': {
-                                        color: 'secondary.main',
-                                        borderColor: 'secondary.main',
-                                    },
-                                }}
-                            >
-                                Login
-                            </Button>
-                            <Button
-                                variant="contained"
-                                LinkComponent={Link}
-                                href={AUTH_URL.REGISTER}
-                                sx={{
-                                    color: 'white',
-                                    borderRadius: '15px',
-                                    bgcolor: 'success.dark',
-                                    '&:hover': {
-                                        bgcolor: 'secondary.main',
-                                    },
-                                }}
-                            >
-                                Sign up
-                            </Button>
+                            {userRoles &&
+                            (userRoles === ROLES.admin.toString() ||
+                                userRoles === ROLES.partner.toString()) ? (
+                                <Button
+                                    variant="outlined"
+                                    LinkComponent={Link}
+                                    href={
+                                        userRoles === ROLES.admin.toString()
+                                            ? ADMIN_URL.HOMEPAGE
+                                            : NURSE_URL.HOMEPAGE
+                                    }
+                                    sx={{
+                                        color: 'success.dark',
+                                        borderColor: 'success.dark',
+                                        borderRadius: '15px',
+                                        '&:hover': {
+                                            color: 'secondary.main',
+                                            borderColor: 'secondary.main',
+                                        },
+                                    }}
+                                >
+                                    Go to dashboard
+                                </Button>
+                            ) : (
+                                <>
+                                    <Button
+                                        variant="outlined"
+                                        LinkComponent={Link}
+                                        href={AUTH_URL.LOGIN}
+                                        sx={{
+                                            color: 'success.dark',
+                                            borderColor: 'success.dark',
+                                            borderRadius: '15px',
+                                            '&:hover': {
+                                                color: 'secondary.main',
+                                                borderColor: 'secondary.main',
+                                            },
+                                        }}
+                                    >
+                                        Login
+                                    </Button>
+                                    <Button
+                                        variant="contained"
+                                        LinkComponent={Link}
+                                        href={AUTH_URL.REGISTER}
+                                        sx={{
+                                            color: 'white',
+                                            borderRadius: '15px',
+                                            bgcolor: 'success.dark',
+                                            '&:hover': {
+                                                bgcolor: 'secondary.main',
+                                            },
+                                        }}
+                                    >
+                                        Sign up
+                                    </Button>
+                                </>
+                            )}
                         </div>
                     </div>
                 </Toolbar>
