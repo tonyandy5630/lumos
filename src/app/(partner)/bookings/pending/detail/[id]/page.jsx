8823bd6a2d826399d2905b3d791f2bd931816644
Bùi Thanh Tú <61198577+tonyandy5630@users.noreply.github.com>
2024-02-24 04:37:12 +0700

    <ui> Add Pending Booking Page

--- src/app/(partner)/bookings/pending/detail/[id]/page.jsx
+++ src/app/(partner)/bookings/pending/detail/[id]/page.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
+import PageTitle from '@/components/PageTitle'
+import PendingBookingDetail from '@/components/Pages/PartnerPendingBookingPage/DetailSection'
+
+export const metadata = {
+    title: 'Pending detail Page',
+}
 
 export default function PendingDetailPage() {
-    return <div>PendingDetailPage</div>
+    return (
+        <>
+            <PageTitle>Booking Detail</PageTitle>
+            <PendingBookingDetail />
+        </>
+    )
 }
