df0a62ac9f91d780b0cf2770ddbc42d0fd475ba5
Bùi Thanh Tú <61198577+tonyandy5630@users.noreply.github.com>
2024-02-27 05:14:06 +0700

    add add partner files

---
+++ src/app/admin/partners/add/page.jsx
@@ -0,0 +1,14 @@
+import PageTitle from '@/components/PageTitle'
+import React from 'react'
+
+export const metadata = {
+    title: 'Add a partner',
+}
+
+export default function AddPartnerPage() {
+    return (
+        <div>
+            <PageTitle>Add New Partner</PageTitle>
+        </div>
+    )
+}
---
+++ src/app/admin/partners/page.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function PartnerAdminPage() {
+    return <div>PartnerAdminPage</div>
+}
