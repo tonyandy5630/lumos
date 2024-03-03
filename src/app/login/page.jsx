29a7b8e70543dc4719967d88e3600f26dd262066
Bùi Thanh Tú <61198577+tonyandy5630@users.noreply.github.com>
2024-03-03 19:40:45 +0700

    <modify> change sign up label to customer

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -70,7 +70,7 @@ export default function LoginPage() {
                                         color: '#107B6C',
                                     }}
                                 >
-                                    New to Lumos?{' '}
+                                    Join as Customer
                                     <Link
                                         href={AUTH_URL.REGISTER}
                                         className="underline text-mosh"
