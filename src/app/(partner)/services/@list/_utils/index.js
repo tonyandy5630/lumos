e1a104536bfc87f50f92e497ad470ada25487a3c
Bùi Thanh Tú <61198577+tonyandy5630@users.noreply.github.com>
2024-03-16 05:27:24 +0700

    <modify> format price in service list

--- src/app/(partner)/services/@list/_utils/index.js
+++ src/app/(partner)/services/@list/_utils/index.js
@@ -1,9 +1,11 @@
+import priceFormat from '@/utils/priceFormat'
+
 export const formatData = (obj) => {
     const { name, price, rating, bookedQuantity, code, serviceId } = obj
     return {
         code,
         name,
-        price,
+        price: priceFormat('vi-VI', 'VND', price),
         rating: rating ?? 0,
         bookedQuantity,
         serviceId,
